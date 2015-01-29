2c42ba41016bec241842dc0abe90b158aa9ee4d5
Tiger <kirill-1025@mail.ru>
2015-01-30 00:14:12 +0700

    tack class

---
+++ ArtInt/Tack.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArtInt
+{
+    class Tack
+    {
+        public int ID = -1;
+
+        public Tack()
+        {
+        }
+
+        public ~Tack()
+        {
+        }
+    }
+}
